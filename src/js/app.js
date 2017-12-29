d0898293810061f7021efb9030c3ed45b097083d
André Karpištšenko <andre@lap.ee>
2017-12-29 20:19:14 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -14,13 +14,3 @@
   });
   /* eslint-enable */
 })();
-
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-111731114-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-111731114-1');
-</script>
