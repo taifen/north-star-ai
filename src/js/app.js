fc0d26c232c1d67ef70a999bea9a3c9d6d596d46
André Karpištšenko <andre@lap.ee>
2017-12-29 20:17:13 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -14,3 +14,13 @@
   });
   /* eslint-enable */
 })();
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-111731114-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-111731114-1');
+</script>
